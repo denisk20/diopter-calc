313ee5615508830884378e0c988eb7215a03b037
Denis Kniazhev <dkniazhev@spredfast.com>
2019-04-20 20:29:48 +0300

    Added fragments.

--- app/src/main/java/org/endmyopia/calc/MainActivity.kt
+++ app/src/main/java/org/endmyopia/calc/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -18,11 +19,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
@@ -30,6 +26,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        val item = nav_view.menu.get(0)
+        item.setChecked(true)
+        onNavigationItemSelected(item)
     }
 
     override fun onBackPressed() {
@@ -57,27 +57,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
+            R.id.measure -> {
+                fragmentTransaction.replace(R.id.content, MeasureFragment())
             }
-            R.id.nav_gallery -> {
-
+            R.id.progress -> {
+                fragmentTransaction.replace(R.id.content, ProgressFragment())
             }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
+            R.id.settings -> {
+                fragmentTransaction.replace(R.id.content, SettingsFragment())
             }
         }
+
+        fragmentTransaction.commit()
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
---
+++ app/src/main/java/org/endmyopia/calc/MeasureFragment.kt
@@ -0,0 +1,19 @@
+package org.endmyopia.calc
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MeasureFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_measure, container, false)
+    }
+}
