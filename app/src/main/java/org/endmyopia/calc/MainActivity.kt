b614c13233e655abdb3ada02a1fd197c4da46fb6
Denis Kniazhev <dkniazhev@spredfast.com>
2019-05-10 11:10:46 +0300

    Removed menu

--- app/src/main/java/org/endmyopia/calc/MainActivity.kt
+++ app/src/main/java/org/endmyopia/calc/MainActivity.kt
@@ -57,22 +57,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
 
