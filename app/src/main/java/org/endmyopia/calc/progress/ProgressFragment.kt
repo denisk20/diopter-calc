90a9989b5993edbd034f249b8d799c66a98301a4
Denis Kniazhev <dkniazhev@spredfast.com>
2019-07-22 00:15:23 +0300

    Observing filter buttons change

--- app/src/main/java/org/endmyopia/calc/progress/ProgressFragment.kt
+++ app/src/main/java/org/endmyopia/calc/progress/ProgressFragment.kt
@@ -4,17 +4,69 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import org.endmyopia.calc.R
+import org.endmyopia.calc.data.MeasurementMode
+import org.endmyopia.calc.databinding.FragmentProgressBinding
 
 
 class ProgressFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentProgressBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progress, container, false)
+        val view = inflater.inflate(R.layout.fragment_progress, container, false)
+
+        dataBinding = FragmentProgressBinding.bind(view)
+        dataBinding.lifecycleOwner = this
+        val holder: ProgressStateHolder = ViewModelProviders.of(activity!!).get(ProgressStateHolder::class.java)
+        dataBinding.holder = holder
+
+        addFilterOnClickListener(dataBinding.filterLeft, MeasurementMode.LEFT)
+        addFilterOnClickListener(dataBinding.filterBoth, MeasurementMode.BOTH)
+        addFilterOnClickListener(dataBinding.filterRight, MeasurementMode.RIGHT)
+
+        holder.selectedModes.observe(this, Observer {
+            processFilterButtonChange(it, dataBinding.filterLeft, MeasurementMode.LEFT)
+            processFilterButtonChange(it, dataBinding.filterBoth, MeasurementMode.BOTH)
+            processFilterButtonChange(it, dataBinding.filterRight, MeasurementMode.RIGHT)
+        })
+
+        return view
+    }
+
+    private fun processFilterButtonChange(
+        it: List<MeasurementMode>,
+        button: Button,
+        mode: MeasurementMode
+    ) {
+        button.setBackgroundTintList(
+            ContextCompat.getColorStateList(
+                context!!,
+                if (it.contains(mode)) R.color.gray else R.color.white
+            )
+        )
+    }
+
+    private fun addFilterOnClickListener(button: Button, mode: MeasurementMode) {
+        button.setOnClickListener {
+            val result: ArrayList<MeasurementMode> = ArrayList(
+                dataBinding.holder?.selectedModes?.value
+            )
+
+            if (result.contains(mode) == true) {
+                result.remove(mode)
+            } else {
+                result.add(mode)
+            }
+            dataBinding.holder?.selectedModes?.postValue(result)
+        }
     }
 }
---
+++ app/src/main/java/org/endmyopia/calc/progress/ProgressStateHolder.kt
@@ -0,0 +1,21 @@
+package org.endmyopia.calc.progress
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.endmyopia.calc.data.MeasurementMode
+
+/**
+ * @author denisk
+ * @since 2019-07-20.
+ */
+class ProgressStateHolder : ViewModel() {
+    val selectedModes: MutableLiveData<List<MeasurementMode>> by lazy {
+        MutableLiveData<List<MeasurementMode>>(
+            arrayListOf()
+        )
+    }
+
+    fun filterEnabled(mode: MeasurementMode): Boolean {
+        return selectedModes.value?.contains(mode) == true
+    }
+}
