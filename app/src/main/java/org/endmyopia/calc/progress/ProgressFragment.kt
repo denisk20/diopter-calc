dec95793c488dcce4f93f92bc8d97703edb8275e
Denys Kniazhev <denys.kniazhev@agileengine.com>
2020-03-22 13:14:27 +0200

    removed debug code

--- app/src/main/java/org/endmyopia/calc/progress/ProgressFragment.kt
+++ app/src/main/java/org/endmyopia/calc/progress/ProgressFragment.kt
@@ -176,31 +176,31 @@ class ProgressFragment : Fragment() {
         GlobalScope.launch {
             dataBinding.holder?.selectedValue?.postValue(null)
             dataBinding.chart.highlightValues(null)
-//            val measurements =
-//                AppDatabase.getInstance(context!!.applicationContext as Application)
-//                    .getMeasurementDao()
-//                    .getMeasurements(modes)
+            val measurements =
+                AppDatabase.getInstance(context!!.applicationContext as Application)
+                    .getMeasurementDao()
+                    .getMeasurements(modes)
 
-            val measurements = listOf(
-                Measurement(1, MeasurementMode.BOTH, 1582520777860, 0.3),
-                Measurement(2, MeasurementMode.BOTH, 1582520780860, 0.79),
-                Measurement(3, MeasurementMode.BOTH, 1582520785860, 0.20),
-                Measurement(4, MeasurementMode.BOTH, 1582520797860, 0.40),
-                Measurement(5, MeasurementMode.BOTH, 1582520975860, 0.50),
-
-                Measurement(1, MeasurementMode.LEFT, 1582520776860, 0.0),
-                Measurement(2, MeasurementMode.LEFT, 1582520777860, 0.61),
-                Measurement(3, MeasurementMode.LEFT, 1582520779860, 0.63),
-                Measurement(4, MeasurementMode.LEFT, 1582520796860, 0.60),
-                Measurement(5, MeasurementMode.LEFT, 1582520974860, 0.65),
-                Measurement(5, MeasurementMode.LEFT, 1582520977860, 0.82),
-
-                Measurement(1, MeasurementMode.RIGHT, 1582520777860, 0.9),
-                Measurement(2, MeasurementMode.RIGHT, 1582520782860, 0.2),
-                Measurement(3, MeasurementMode.RIGHT, 1582520787860, 0.4),
-                Measurement(4, MeasurementMode.RIGHT, 1582520799860, 0.3),
-                Measurement(5, MeasurementMode.RIGHT, 1582520984860, 0.7)
-            )
+//            val measurements = listOf(
+//                Measurement(1, MeasurementMode.BOTH, 1582520777860, 0.3),
+//                Measurement(2, MeasurementMode.BOTH, 1582520780860, 0.79),
+//                Measurement(3, MeasurementMode.BOTH, 1582520785860, 0.20),
+//                Measurement(4, MeasurementMode.BOTH, 1582520797860, 0.40),
+//                Measurement(5, MeasurementMode.BOTH, 1582520975860, 0.50),
+//
+//                Measurement(1, MeasurementMode.LEFT, 1582520776860, 0.0),
+//                Measurement(2, MeasurementMode.LEFT, 1582520777860, 0.61),
+//                Measurement(3, MeasurementMode.LEFT, 1582520779860, 0.63),
+//                Measurement(4, MeasurementMode.LEFT, 1582520796860, 0.60),
+//                Measurement(5, MeasurementMode.LEFT, 1582520974860, 0.65),
+//                Measurement(5, MeasurementMode.LEFT, 1582520977860, 0.82),
+//
+//                Measurement(1, MeasurementMode.RIGHT, 1582520777860, 0.9),
+//                Measurement(2, MeasurementMode.RIGHT, 1582520782860, 0.2),
+//                Measurement(3, MeasurementMode.RIGHT, 1582520787860, 0.4),
+//                Measurement(4, MeasurementMode.RIGHT, 1582520799860, 0.3),
+//                Measurement(5, MeasurementMode.RIGHT, 1582520984860, 0.7)
+//            )
             with(modes) {
                 if (contains(MeasurementMode.RIGHT)) createDataSet(
                     measurements,
